9ac3238492783c9b7830635261a319298a41db85
Moises <moisescamposdaw@gmail.com>
2024-10-17 15:26:05 +0100

    Error de muestra de fechas en un solo mes corregido

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 const festivos = [
     // YYYY-MM-DD
     {isla:"La Palma", 
-        dia:[5, 15], 
-        mes:[8, 8],
+        dia:[5, 15, 12, 8], 
+        mes:[8, 8, 2, 9],
         celebracion: ["Nuestra Señora de las Nieves", 
-            "La Fiesta de la Morena"],
+            "La Fiesta de la Morena",
+            "Día de Los Indianos",
+            "Danza del Diablo"],
         descripcion: ["Es un culto que llegó a La Palma tras la conquista de la isla en el siglo XV", 
-            "Se remontan al siglo XVI, cuando las mujeres del municipio se reunían para vender o intercambiar el pescado capturado"],
-        costumbres: ["Cada 5 años se realiza una bajada de la virgen al templo en S/C La Palma desde su santuario acompañada de manifestaciones culturales como la Danza de los Enanos", 
-            "Los asistentes visten cholas y bañadores, disfrutando del ambiente festivo en la playa. Destaca el 'baile de la Morena', que se realiza en la arena de la misma playa."]
+            "Se remontan al siglo XVI, cuando las mujeres del municipio se reunían para vender o intercambiar el pescado capturado",
+            "Sus orígenes se remontan al siglo XIX, cuando se recibía con festejos a los isleños que habían emigrado a Cuba buscando las Américas y volvían presumiendo de las riquezas que habían logrado en su aventura.",
+            "toma el protagonismo, una antigua costumbre que refleja la lucha entre el bien, representado por la Virgen de la Candelaria, y el mal, personificado por el Diablo."],
+        costumbres: ["Cada 5 años se realiza una bajada de la virgen al templo en S/C La Palma desde su santuario acompañada de manifestaciones culturales como la Danza de los Enanos",
+            "Los asistentes visten cholas y bañadores, disfrutando del ambiente festivo en la playa. Destaca el 'baile de la Morena', que se realiza en la arena de la misma playa.",
+            "Comienza a las 12:00 de la mañana en un acto conocido como “La Espera”, con el que se hace un recibimiento a los indianos.Desde entonces, durante toda la jornada, sonará la música cubana y correrá la caña de azúcar, la melaza y los mojitos.",
+            "El protagonista es una figura maléfica, que escupe fuegos artificiales por todas partes. Su llegada la anuncian una serie de gigantes y cabezudos que interrumpen la verbena sin previo aviso."]
     },
     {isla:"El Hierro", 
         dia:[24], 
@@ -32,11 +38,11 @@ const festivos = [
         costumbres: ["Destaca el rito de la procesión de las candelas, donde el fuego es protagonista."]
     },
     {isla:"Gran Canaria", 
-        dia:[], 
-        mes:[],
-        celebracion: [],
-        descripcion: [],
-        costumbres: []
+        dia:[9], 
+        mes:[9],
+        celebracion: ["Nuestra Señora del Pino."],
+        descripcion: ["Es una de las advocaciones marianas que representan a la Virgen María. Ubicada en la Basílica del municipio de Teror"],
+        costumbres: ["Se celebra la Romería del Pino, una colorida procesión que reúne a participantes de todos los municipios de la isla y de las diferentes islas del archipiélago canario."]
     },
     {isla:"Lanzarote", 
         dia:[16], 
@@ -64,17 +70,21 @@ function volver(){
 function pintaFestivos(numIsla) {
     let mes = localStorage.getItem("mes");
     let isla = festivos[numIsla];
-    let contador = isla.dia.length;
-    let aux = contador;
+    let indices = [];
 
-    if (mes==isla.mes[contador-aux]) {
-        for (let i = 0; i <= 4; i++) {
-            for (let j = 0; j <= 6; j++) { 
-                let valor = document.getElementById(i+"."+j).value;
-                if (valor==isla.dia[contador-aux] && valor!=null) {
-                    document.getElementById(i+"."+j).title = isla.celebracion[contador-aux];
-                    document.getElementById(i+"."+j).classList.add("festivo");
-                    aux--;
+    for (let i = 0; i < isla.mes.length; i++) {
+        if (mes==isla.mes[i]) {
+            indices.push(i);
+        }
+    }
+
+    for (let i = 0; i < indices.length; i++) {
+        for (let j = 0; j <= 4; j++) {
+            for (let k = 0; k <= 6; k++) { 
+                let valor = document.getElementById(j+"."+k).value;
+                if (valor==isla.dia[indices[i]] && valor!=null) {
+                    document.getElementById(j+"."+k).title = isla.celebracion[indices[i]];
+                    document.getElementById(j+"."+k).classList.add("festivo");
                 }
             }
         }
@@ -87,7 +97,7 @@ function escribeFestivos(numIsla){
     let isla = festivos[numIsla];
 
     for (let i = 0; i < isla.dia.length; i++) {
-        str = "<tr><td>"+isla.celebracion[i]+"</td><td>"+isla.mes[i]+"</td><td>"+isla.dia[i]+"</td><td>"+isla.descripcion[i]+"</td><td>"+isla.costumbres[i]+"</td></tr>";
+        str = "<tr><td>"+isla.celebracion[i]+"</td><td>"+isla.dia[i]+"</td><td>"+isla.mes[i]+"</td><td>"+isla.descripcion[i]+"</td><td>"+isla.costumbres[i]+"</td></tr>";
         document.getElementById("diasFestivos").innerHTML += str;
     }
 }
